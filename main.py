aca32daae9068333d6309bca914833ad235f5bf5
deguchi.yuka <piyonoii3@hotmail.co.jp>
2018-12-02 18:39:49 +0900

    add main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+class Main():
+    def __init__(self):
+        print("girlfriend: あなたの名前、教えて？");
+
+    def Response(self):
+        your_name = input("your_name: ");
+        print("おはよ、" + your_name);
+
+main = Main();
+main.Response();
